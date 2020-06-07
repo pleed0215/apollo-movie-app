42dc9be9ea3f85b0638577519800161fc14a20e2
pleed0215 <pleed0215@icloud.com>
2020-06-07 15:47:14 +0900

    gh-pages deployment added

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://movieql.herokuapp.com/",
   defaultOptions: {
     watchQuery: {
       fetchPolicy: "cache-and-network",
